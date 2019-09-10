88c3915c7be1b1d9c9cfbb3220000afc1ad5b4c3
Vitas1968 <dor1968@mail.ru>
2019-09-10 21:53:45 +0700

    добавлен приоритет и возможность работы с ожидаемыми исключения

--- src/main/java/Lesson7_Ref_Ann/Test.java
+++ src/main/java/Lesson7_Ref_Ann/Test.java
@@ -5,8 +5,17 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Test
 {
+    static class None extends Throwable {
+        private static final long serialVersionUID = 1L;
+
+        private None() {
+        }
+    }
+    Class<? extends Throwable> expected() default None.class;
+
+    int priority() default 5;
 }
