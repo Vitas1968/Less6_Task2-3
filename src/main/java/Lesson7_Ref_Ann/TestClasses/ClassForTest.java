03aa89d3dc9bc136b4acbde089c42a9e46c667a2
Vitas1968 <dor1968@mail.ru>
2019-09-11 23:11:41 +0700

    добавил описание проиритетов

--- src/main/java/Lesson7_Ref_Ann/TestClasses/ClassForTest.java
+++ src/main/java/Lesson7_Ref_Ann/TestClasses/ClassForTest.java
@@ -1,13 +1,14 @@
 package Lesson7_Ref_Ann.TestClasses;
 
 import Lesson7_Ref_Ann.Annotations.AfterSuite;
+import Lesson7_Ref_Ann.Annotations.BeforeSuite;
 import Lesson7_Ref_Ann.Annotations.Test;
 
 import java.lang.reflect.Method;
 
 public class ClassForTest
 {
-    @AfterSuite
+    @BeforeSuite
     public void startTest()
     {
         System.out.println("Выполнится первым");
@@ -15,18 +16,18 @@ public class ClassForTest
     @Test
     public void testMethod1()
     {
-        System.out.println("Выполняется testMethod1 в ClassForTest");
+        System.out.println("Выполняется testMethod1 в ClassForTest. Приоритет по умолчанию 5");
     }
-    @Test(priority = 8)
+    @Test(priority = 2)
     public void testMethod2()
     {
-        System.out.println("Выполняется testMethod2 в ClassForTest");
+        System.out.println("Выполняется testMethod2 в ClassForTest. Приоритет 2");
     }
 
-    @Test(priority = 2)
+    @Test(priority = 8)
     public void testMethod3()
     {
-        System.out.println("Выполняется testMethod3 в ClassForTest");
+        System.out.println("Выполняется testMethod3 в ClassForTest. Приоритет 8");
     }
     @AfterSuite
     public void endTest()
@@ -34,10 +35,10 @@ public class ClassForTest
         System.out.println("Выполнится последним");
     }
 
-    @AfterSuite
-    public void endTest1()
-    {
-        System.out.println("Не выполнится совсем");
-    }
+//    @AfterSuite
+//    public void endTest1()
+//    {
+//        System.out.println("Не выполнится совсем");
+//    }
 
 }
